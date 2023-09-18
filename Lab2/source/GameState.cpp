9c2557b7068d4fdaa9180f02932c47aa4baa7fee
Lvl3Mage <lvlthreemage@gmail.com>
2023-09-18 23:15:50 +0200

    Lab2 Coin Random Generation

--- Lab2/source/GameState.cpp
+++ Lab2/source/GameState.cpp
@@ -1,16 +1,26 @@
 #include "GameState.H"
-
+#include <cstdlib> // for rand() and srand() functions
+#include <ctime> // for time() function to seed rand()
 GameState::GameState()
 { }
-	
+int* GenRandomPosition(){
+	int* coords = new int[2];
+	// Seed the random number generator with the current time
+	std::srand(static_cast<unsigned int>(std::time(nullptr)));
+	int N = 23;
+	coords[0] = std::rand() % (N + 1);
+	coords[1] = std::rand() % (N + 1);
+	return coords;
+}
 void GameState::Reset()
 {
 	player1_row     = 0;
 	player1_column  = 0;
 	player2_row     = 0;
 	player2_column  = 0;
-	coin_row       = 10;
-	coin_column    = 10;
+	int* coords = GenRandomPosition();
+	coin_row       = coords[0];
+	coin_column    = coords[1];
 	player1CoinCollected = false;
 	player2CoinCollected = false;
 }
