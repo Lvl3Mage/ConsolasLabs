418c290f555f5fd2c2176eb36b98d4275dd4fc06
Karl <lvlthreemage@gmail.com>
2023-10-03 19:17:29 +0200

    revert to 6.6

--- Lab4/source/program.c
+++ Lab4/source/program.c
@@ -2,8 +2,6 @@
 #include <nds.h>
 #include "testImage.h"
 #include "testImage2.h"
-#include "amongus.h"
-#include "berlin.h"
 # define TIMER_SPEED ( BUS_CLOCK /1024)
 uint ticks = 0;
 void RunTimer(){
@@ -12,52 +10,70 @@ void RunTimer(){
 int GetSeconds(){
 	return ticks/TIMER_SPEED;
 }
+
+void DrawSquare(int coords[], int size){
+	unsigned short *fbA = VRAM_A;
+	for(int i = coords[1]; i < coords[1]+size; i++){
+		for(int j = coords[0]; j < coords[0]+size; j++){
+			fbA[i*256 + j] = RGB15(31,31,31);
+		}
+	}
+}
+void ClearDisplay(){
+	unsigned short *fbA = VRAM_A;
+	for(int i = 0; i < 192; i++){
+		for(int j = 0; j < 256; j++){
+			fbA[i*256 + j] = RGB15(0,0,0);
+		}
+	}
+}
+int Clamp(int val, int min, int max){
+	if(val < min){
+		val = min;
+	}
+	else if (val > max){
+		val = max;
+	}
+	return val;
+}
 int main (void)
 {
 	timerStart (0 , ClockDivider_1024 , 0 , NULL ) ; 
 	REG_POWERCNT = POWER_LCD | POWER_2D_A;
 
+
 	VRAM_A_CR    = VRAM_ENABLE | VRAM_A_LCD;
-	dmaCopy(testImageBitmap, VRAM_A, testImageBitmapLen);
 
-	VRAM_B_CR    = VRAM_ENABLE | VRAM_B_LCD;
-	dmaCopy(testImage2Bitmap, VRAM_B, testImage2BitmapLen);
-
-	VRAM_C_CR    = VRAM_ENABLE | VRAM_C_LCD;
-	dmaCopy(amongusBitmap, VRAM_B,amongusBitmapLen);
-	VRAM_D_CR    = VRAM_ENABLE | VRAM_D_LCD;
-	dmaCopy(berlinBitmap, VRAM_D, berlinBitmapLen);
 
 	REG_DISPCNT  = MODE_FB0;
-	int i = 0;
+
+	int coords[2] = {32,32};
+	int size = 50;
 	while (1)
 	{
 		RunTimer();
 		consoleClear();
 		scanKeys();
-		int down=keysDown();
-		if(down & KEY_X){
-			i++;
-			if(i >= 4){
-				i = 0;
-			}
+		int held=keysHeld();
+		ClearDisplay();
+		DrawSquare(coords, size);
+		if (held & KEY_UP){
+			coords[1] -= 1;
 		}
-		if (i % 4 == 0)
-		{
-			REG_DISPCNT  = MODE_FB0;
+		if (held & KEY_RIGHT){
+			coords[0] += 1;
+
 		}
-		if (i % 4 == 1)
-		{
-			REG_DISPCNT  = MODE_FB1;
+		if (held & KEY_DOWN){
+			coords[1] += 1;
+
 		}
-		if (i % 4 == 2)
-		{
-			REG_DISPCNT  = MODE_FB2;
+		if (held & KEY_LEFT){
+			coords[0] -= 1;
+
 		}
-		if (i % 4 == 3)
-		{
-			REG_DISPCNT  = MODE_FB3;
-		}
+		coords[0] = Clamp(coords[0],0,256-size);
+		coords[1] = Clamp(coords[1],0,192-size);
 		swiWaitForVBlank();
 	}
 	return 0;
