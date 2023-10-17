b4a025ce26897edf70e247146a0b682382143fb2
Karl <lvlthreemage@gmail.com>
2023-10-17 18:37:50 +0200

    Lab6 7.5

--- Lab6/source/program.cpp
+++ Lab6/source/program.cpp
@@ -1,73 +1,51 @@
 #include <nds.h>
 #include <stdio.h>
 
-u8 comecocos[64] =
+u8 green[64] =
 {
-    2,2,1,1,1,1,2,2,
-    2,1,1,1,1,1,2,2,
-    1,1,1,1,1,2,2,2,
-    1,1,1,1,2,2,2,2,
-    1,1,1,1,2,2,2,2,
-    1,1,1,1,1,2,2,2,
-    2,1,1,1,1,1,2,2,
-    2,2,1,1,1,1,2,2,
+  1,1,1,1,1,1,1,1,
+  1,1,1,1,1,1,1,1,
+  1,1,1,1,1,1,1,1,
+  1,1,1,1,1,1,1,1,
+  1,1,1,1,1,1,1,1,
+  1,1,1,1,1,1,1,1,
+  1,1,1,1,1,1,1,1,
+  1,1,1,1,1,1,1,1,
 };
 
-u8 fondo[64] =
+u8 red[64] =
 {
-	2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2
+  2,2,2,2,2,2,2,2,
+  2,2,2,2,2,2,2,2,
+  2,2,2,2,2,2,2,2,
+  2,2,2,2,2,2,2,2,
+  2,2,2,2,2,2,2,2,
+  2,2,2,2,2,2,2,2,
+  2,2,2,2,2,2,2,2,
+  2,2,2,2,2,2,2,2,
+};
+u8 blue[64] =
+{
+  3,3,3,3,3,3,3,3,
+  3,3,3,3,3,3,3,3,
+  3,3,3,3,3,3,3,3,
+  3,3,3,3,3,3,3,3,
+  3,3,3,3,3,3,3,3,
+  3,3,3,3,3,3,3,3,
+  3,3,3,3,3,3,3,3,
+  3,3,3,3,3,3,3,3,
 };
 
-u8 fantasma[64] =
+u8 yellow[64] =
 {
-	2,2,2,3,3,2,2,2,
-	2,3,3,3,3,3,3,2,
-	3,3,3,3,3,3,3,3,
-	3,4,2,3,3,4,2,3,
-	3,4,2,3,3,4,2,3,
-	3,3,3,3,3,3,3,3,
-	3,3,3,3,3,3,3,3,
-	3,2,3,2,2,3,2,3
-};
-
-u16 mapData[768] =
-{
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2
+  4,4,4,4,4,4,4,4,
+  4,4,4,4,4,4,4,4,
+  4,4,4,4,4,4,4,4,
+  4,4,4,4,4,4,4,4,
+  4,4,4,4,4,4,4,4,
+  4,4,4,4,4,4,4,4,
+  4,4,4,4,4,4,4,4,
+  4,4,4,4,4,4,4,4,
 };
 int Clamp(int val, int min, int max){
 	if(val < min){
@@ -80,94 +58,59 @@ int Clamp(int val, int min, int max){
 }
 int main( void )
 {
-	int fila,columna,pos_mapMemory,pos_mapData;
+	REG_POWERCNT = POWER_ALL_2D;
+	REG_DISPCNT  = MODE_0_2D | DISPLAY_BG0_ACTIVE | DISPLAY_BG1_ACTIVE | DISPLAY_BG2_ACTIVE | DISPLAY_BG3_ACTIVE;
+	VRAM_A_CR    = VRAM_ENABLE | VRAM_A_MAIN_BG;
+	BGCTRL[0]    = BG_32x32 | BG_COLOR_256 | BG_MAP_BASE(0) | BG_TILE_BASE(1) | BG_PRIORITY(3);  // Bottom
+	BGCTRL[1]    = BG_32x32 | BG_COLOR_256 | BG_MAP_BASE(1) | BG_TILE_BASE(2) | BG_PRIORITY(2);  // Top
+	BGCTRL[2]    = BG_32x32 | BG_COLOR_256 | BG_MAP_BASE(2) | BG_TILE_BASE(3) | BG_PRIORITY(1);  // Top
+	BGCTRL[3]    = BG_32x32 | BG_COLOR_256 | BG_MAP_BASE(3) | BG_TILE_BASE(4) | BG_PRIORITY(0);  // Top
 
-	REG_POWERCNT    = POWER_ALL_2D;
-	REG_DISPCNT     = MODE_0_2D | DISPLAY_BG0_ACTIVE;
-	REG_DISPCNT_SUB = MODE_0_2D | DISPLAY_BG0_ACTIVE;
-	VRAM_A_CR       = VRAM_ENABLE | VRAM_A_MAIN_BG;
-	VRAM_C_CR       = VRAM_ENABLE | VRAM_C_SUB_BG;
-	BGCTRL[0]       = BG_32x32 | BG_COLOR_256 | BG_MAP_BASE(0) | BG_TILE_BASE(1);
-	BGCTRL_SUB[0]   = BG_32x32 | BG_COLOR_256 | BG_MAP_BASE(0) | BG_TILE_BASE(1);
+	static u8*  tileMemoryB0  = (u8*)  BG_TILE_RAM(1);
+	static u16* mapMemoryB0   = (u16*) BG_MAP_RAM(0);
 
-	
-	static u8*  tileMemory    = (u8*)  BG_TILE_RAM(1);
-	static u16* mapMemory     = (u16*) BG_MAP_RAM(0);
-	static u8*  tileMemorySub = (u8*)  BG_TILE_RAM_SUB(1);
-	static u16* mapMemorySub  = (u16*) BG_MAP_RAM_SUB(0);
+	static u8*  tileMemoryB1 = (u8*)  BG_TILE_RAM(2);
+	static u16* mapMemoryB1  = (u16*) BG_MAP_RAM(1);
 
-	BG_PALETTE[1]=RGB15(28,0,0);
-	BG_PALETTE[2]=RGB15(0,20,0);
-	BG_PALETTE_SUB[1] = RGB15(0,20,0);
-	BG_PALETTE_SUB[2] = RGB15(20,0,0);
+	static u8*  tileMemoryB2 = (u8*)  BG_TILE_RAM(3);
+	static u16* mapMemoryB2  = (u16*) BG_MAP_RAM(2);
 
-	dmaCopy(comecocos,tileMemory + 64,  sizeof(comecocos));
-	dmaCopy(fondo,  tileMemory + 128, sizeof(fondo));
-	dmaCopy(comecocos,tileMemorySub + 64,  sizeof(comecocos));
-	dmaCopy(fondo,  tileMemorySub + 128, sizeof(fondo));
-	// dmaCopy(fantasma,  tileMemory + 192, sizeof(fantasma));
+	static u8*  tileMemoryB3 = (u8*)  BG_TILE_RAM(4);
+	static u16* mapMemoryB3  = (u16*) BG_MAP_RAM(3);
 
-	pos_mapData = 0;
+	BG_PALETTE[1]     = RGB15(0,20,0);
+	BG_PALETTE[2]     = RGB15(20,0,0);
+	BG_PALETTE[3]     = RGB15(0,0,20);
+	BG_PALETTE[4]     = RGB15(20,20,0);
+
+	dmaCopy(green, tileMemoryB0 + 64,  sizeof(green));
+
+	dmaCopy(red, tileMemoryB1 + 64,  sizeof(red));
+
+	dmaCopy(blue, tileMemoryB2 + 64,  sizeof(blue));
+
+	dmaCopy(yellow, tileMemoryB3 + 64,  sizeof(yellow));
+
+	int fila, columna;
+
 	for(fila=0;fila<24;fila++){
 		for(columna=0;columna<32;columna++)
 		{
-			pos_mapMemory            = fila*32+columna;
-			mapMemory[pos_mapMemory] = mapData[pos_mapData];
-			pos_mapData ++;
-	    }
+			mapMemoryB0[fila*32+columna] = 1;       // Fondo 0, todo verde
+			if (columna>=8){
+				mapMemoryB1[fila*32+columna] = 1;   // Fondo 1, mitad derecha rojo
+			}
+			if (columna>=16){
+				mapMemoryB2[fila*32+columna] = 1;   // Fondo 1, mitad derecha rojo
+			}
+			if (columna>=24){
+				mapMemoryB3[fila*32+columna] = 1;   // Fondo 1, mitad derecha rojo
+			}
+		}
 	}
-	pos_mapData = 0;
-	for(fila=0;fila<24;fila++){
-		for(columna=0;columna<32;columna++)
-		{
-			pos_mapMemory            = fila*32+columna;
-			mapMemorySub[pos_mapMemory] = mapData[pos_mapData];
-			pos_mapData ++;
-	    }
-	}
-	int posX = 0;
-	int posY = 0;
-	bool upperScreen = false;
+
 	while(1)
 	{
-		scanKeys();
-		u32 keys = keysDown();
-
-		mapMemory[posY*32 + posX] = 2;
-		mapMemorySub[posY*32 + posX] = 2;
-		if(keys & KEY_DOWN){
-			posY ++;
-		}
-		if(keys & KEY_UP){
-			posY --;
-		}
-		if(keys & KEY_LEFT){
-			posX --;
-		}
-		if(keys & KEY_RIGHT){
-			posX ++;
-		}
-		if(keys & KEY_A){
-			BG_PALETTE[1]=RGB15(10,0,0);
-			BG_PALETTE[2]=RGB15(10,20,0);
-			upperScreen = true;
-		}
-		if(keys & KEY_X){
-			BG_PALETTE[1]=RGB15(28,0,0);
-			BG_PALETTE[2]=RGB15(0,20,0);
-			upperScreen = false;
-		}
-		posX = Clamp(posX, 0, 31);
-		posY = Clamp(posY, 0, 23);
-		if(upperScreen){
-			mapMemorySub[posY*32 + posX] = 1;
-
-		}
-		else{
-			mapMemory[posY*32 + posX] = 1;
-
-		}
-
 		swiWaitForVBlank();
 	}
 }
