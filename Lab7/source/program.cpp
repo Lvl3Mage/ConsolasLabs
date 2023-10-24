01b19e155e4a33a44b8a1ba627f7c6ebb5925938
Karl <lvlthreemage@gmail.com>
2023-10-24 16:34:30 +0200

    Lab7 9.3

--- Lab7/source/program.cpp
+++ Lab7/source/program.cpp
@@ -1,5 +1,6 @@
 #include <nds.h>
 #include <stdio.h>
+#include <bitset>
 class LogState
 {
 	int accumulatedLines = 0;
@@ -12,15 +13,22 @@ public:
 		accumulatedLines = 0;
 	}
 };
+
 int main(void)
 {
+	int counter = 0;
 	// logState = new LogState();
 	consoleDemoInit();
+	u16 lastVal;
 	while(1) 
 	{
-		while (REG_KEYINPUT != 0x03FD)
-			iprintf("\x1b[6;0H Esperando Boton B       ");
-		iprintf("\x1b[6;0HBoton B pulsado             ");
+		u16 val = ~REG_KEYINPUT & 0b0000000011000011;
+		if (val && val != lastVal){
+			counter++;	
+		}
+		lastVal = val;
+		// std::bitset<16> byte1 = std::bitset<16>(~REG_KEYINPUT & 0b0000000011000011); // byte1.to_string().c_str()
+		iprintf("\x1b[6;0H %d", counter);
 		swiWaitForVBlank();
 	}
 }
