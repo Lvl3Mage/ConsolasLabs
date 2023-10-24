5f66b2e002de8da4cc8c9f1a7680e78dd83e4b95
Karl <lvlthreemage@gmail.com>
2023-10-24 17:37:56 +0200

    Lab7 9.8

--- Lab7/source/program.cpp
+++ Lab7/source/program.cpp
@@ -1,23 +1,33 @@
 #include <nds.h>
 #include <stdio.h>
-int counter = 0;
+int counter1 = 0;
+int counter2 = 0;
 
-void int_timer() 
+void int_timer1() 
 {
-	counter ++;
+	counter1 ++;
+}
+void int_timer2(){
+	counter2 ++;
 }
 
 int main(void) 
 {
 	consoleDemoInit();
 	irqEnable(IRQ_TIMER0);
-	irqSet(IRQ_TIMER0,int_timer); 
+	irqEnable(IRQ_TIMER1);
+	irqSet(IRQ_TIMER0,int_timer1); 
+	irqSet(IRQ_TIMER1,int_timer2); 
 
-	TIMER_DATA(0)=49152; 
+	TIMER_DATA(0)=32768; 
 	TIMER_CR(0) = TIMER_DIV_1024 | TIMER_ENABLE | TIMER_IRQ_REQ ;
+
+	TIMER_DATA(1)=0; 
+	TIMER_CR(1) = TIMER_DIV_1024 | TIMER_ENABLE | TIMER_IRQ_REQ ;
 	while(1) 
 	{
-		iprintf("\x1b[12;2H%2i", counter);
+		iprintf("\x1b[12;2H%2i", counter1);
+		iprintf("\x1b[14;2H%2i", counter2);
 		swiWaitForVBlank();
 	}
 }
